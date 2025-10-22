36a069fabc9f3d3de8e1987ad35319ad0be95795
Leo Lima <leonardolimabranco@gmail.com>
2025-10-22 19:05:51 -0300

    docs: update no 'VocavularyColletion.java'

--- backend-java/src/main/java/com/ailinguo/iterator/VocabularyCollection.java
+++ backend-java/src/main/java/com/ailinguo/iterator/VocabularyCollection.java
@@ -3,7 +3,10 @@ package com.ailinguo.iterator;
 import com.ailinguo.model.VocabularyWord;
 
 public interface VocabularyCollection {
+
     IIterator createIterator();
+
     void addWord(VocabularyWord word);
+    
     int size();
 }
